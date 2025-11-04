67a56a7f84decac6a83987dd55435c85e812eff9
sakshammishraGla <sakshammishra5thsep@gmail.com>
2025-11-04 15:25:50 +0530

    changes in ui

--- script.js
+++ script.js
@@ -1,110 +1,121 @@
 $(function () {
     let userSelected = null;
-    let pcSelected = null;  // Fixed typo: pcChoise -> pcSelected
+    let pcSelected = null;
     let userScore = 0;
     let pcScore = 0;
-    let winner;
+    let winner = "";
 
-
-
-    // Update score display function
     function updateScoreDisplay() {
-        $("#computer_score").text(pcScore);  // Fixed: was userScore, should be pcScore
-        $("#user_score").text(userScore);    // Add this for user score
+        $("#computer_score").text(pcScore);
+        $("#user_score").text(userScore);
         $("#winner_message").text(winner);
+    }
+
+    function resetRings() {
+        $(".you_picked .hand, .pc_picked_hand").removeClass("ring-win");
     }
 
     function playAgain() {
         userSelected = null;
         pcSelected = null;
+        resetRings();
     }
 
-    function updateSelectedIcon(iconSelected,who) {
-        if(who=="user"){            
-            switch (iconSelected) {
-                case "scissor": $(".user_selected_Icon").addClass("fa-hand-scissors")
-                    $(".user_selected_Icon").removeClass("fa-hand-fist")
-                    $(".user_selected_Icon").removeClass("fa-hand")
-                    break;
-                case "paper": $(".user_selected_Icon").addClass("fa-hand")
-                    $(".user_selected_Icon").removeClass("fa-hand-scissors")
-                    $(".user_selected_Icon").removeClass("fa-hand-fist")
-                    break;
-                default: $(".user_selected_Icon").addClass("fa-hand-fist")
-                    $(".user_selected_Icon").removeClass("fa-hand-scissors")
-                    $(".user_selected_Icon").removeClass("fa-hand")
-                    break;
-            }
+    function updateSelectedIcon(iconSelected, who) {
+        if (who === "user") {
+            const el = $(".user_selected_Icon");
+            el.removeClass("fa-hand fa-hand-fist fa-hand-scissors");
+            if (iconSelected === "paper") el.addClass("fa-hand");
+            else if (iconSelected === "scissor") el.addClass("fa-hand-scissors");
+            else el.addClass("fa-hand-fist");
+        } else {
+            const el = $(".pc_selected_Icon");
+            el.removeClass("fa-hand fa-hand-fist fa-hand-scissors");
+            if (iconSelected === "paper") el.addClass("fa-hand");
+            else if (iconSelected === "scissor") el.addClass("fa-hand-scissors");
+            else el.addClass("fa-hand-fist");
         }
-        else{
-             switch (iconSelected) {
-                case "scissor": $(".pc_selected_Icon").addClass("fa-hand-scissors")
-                    $(".pc_selected_Icon").removeClass("fa-hand-fist")
-                    $(".pc_selected_Icon").removeClass("fa-hand")
-                    break;
-                case "paper": $(".pc_selected_Icon").addClass("fa-hand")
-                    $(".pc_selected_Icon").removeClass("fa-hand-scissors")
-                    $(".pc_selected_Icon").removeClass("fa-hand-fist")
-                    break;
-                default: $(".pc_selected_Icon").addClass("fa-hand-fist")
-                    $(".pc_selected_Icon").removeClass("fa-hand-scissors")
-                    $(".pc_selected_Icon").removeClass("fa-hand")
-                    break;
-        }
-        console.log(who,iconSelected)
     }
-}
 
-  
     // Initial score display
     updateScoreDisplay();
 
-    $(".hand").click(function () {
-        if ($(this).hasClass("stone")) {
-            userSelected = "stone";
-        } else if ($(this).hasClass("paper")) {
-            userSelected = "paper";
-        } else if ($(this).hasClass("scissor")) {
-            userSelected = "scissor";
-        } else {
-            userSelected = null;
-            console.log("No valid choice selected!");
-            return;
-        }
+    $(".hand_selection_section .hand").on("click", function () {
+        if ($(this).hasClass("stone")) userSelected = "stone";
+        else if ($(this).hasClass("paper")) userSelected = "paper";
+        else if ($(this).hasClass("scissor")) userSelected = "scissor";
+        else return;
 
         $(".hand_selection_section").addClass("hideSection");
         $(".selected_section").removeClass("hideSection");
         pcSelected = getComputerChoice();
         winner = calculateWinner(userSelected, pcSelected);
-        updateSelectedIcon(userSelected,"user");
-        updateSelectedIcon(pcSelected,"pc");
-        // UPDATE THE DISPLAY AFTER EACH GAME
+        updateSelectedIcon(userSelected, "user");
+        updateSelectedIcon(pcSelected, "pc");
+
+        resetRings();
+        if (winner === "User Wins") {
+            $(".you_picked .hand").addClass("ring-win");
+            $("#winner_message").html("<span class='big'>YOU WIN</span><span class='sub'>AGAINST PC</span>");
+            $("#play_Again").text("PLAY AGAIN");
+            $("#next_btn").removeClass("hideSection");
+        } else if (winner === "PC Wins") {
+            $(".pc_picked_hand").addClass("ring-win");
+            $("#winner_message").html("<span class='big'>YOU LOST</span><span class='sub'>AGAINST PC</span>");
+            $("#play_Again").text("PLAY AGAIN");
+            $("#next_btn").addClass("hideSection");
+        } else {
+            $("#winner_message").html("<span class='big'>TIE UP</span>");
+            $("#play_Again").text("REPLAY");
+            $("#next_btn").addClass("hideSection");
+        }
+
         updateScoreDisplay();
     });
 
-    $("#play_Again").click(function () {
-        playAgain()
+    $("#play_Again").on("click", function () {
+        playAgain();
         $(".selected_section").addClass("hideSection");
-        $(".hand_selection_section").removeClass("hideSection")
+        $(".hand_selection_section").removeClass("hideSection");
+        $("#next_btn").addClass("hideSection");
+    });
 
-    })
+    // Rules modal
+    $("#rules_btn").on("click", function(){
+        $("#rules_modal").removeClass("hideSection");
+    });
+    // Next navigates to win screen
+    $("#next_btn").on("click", function(){
+        $("#win_screen").removeClass("hideSection");
+    });
+    // Play again from win screen
+    $("#play_again_win").on("click", function(){
+        $("#win_screen").addClass("hideSection");
+        $("#play_Again").trigger("click");
+    });
+    // Close rules (delegated to handle any DOM changes)
+    $(document).on("click", ".close_rules, .close_rules i", function(e){
+        e.preventDefault();
+        e.stopPropagation();
+        $("#rules_modal").addClass("hideSection");
+    });
+    // click outside the card closes the modal
+    $(document).on("click", "#rules_modal", function(e){
+        if (e.target === this) $(this).addClass("hideSection");
+    });
+    // ESC key closes modal
+    $(document).on("keydown", function(e){
+        if (e.key === "Escape") $("#rules_modal").addClass("hideSection");
+    });
 
     function calculateWinner(user, pc) {
-        if (user == pc) {
-            return "It's a tie";
+        if (user === pc) return "Tie";
+        if ((user === "stone" && pc === "scissor") ||
+            (user === "paper" && pc === "stone") ||
+            (user === "scissor" && pc === "paper")) {
+            userScore++; return "User Wins";
         }
-
-        // User wins conditions
-        if ((user == "stone" && pc == "scissor") ||
-            (user == "paper" && pc == "stone") ||
-            (user == "scissor" && pc == "paper")) {
-            userScore++;
-            return "User Wins";
-        }
-
-        // PC wins (all other cases)
-        pcScore++;
-        return "PC Wins";
+        pcScore++; return "PC Wins";
     }
 
     function getComputerChoice() {
